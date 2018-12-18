e72f694b85a4b20f585a1ec3f056691e2c587597
JornRymenans <45791970+jornrymenans@users.noreply.github.com>
2018-12-18 13:29:57 +0100

    ModelAttribute voor producttable

--- src/main/java/jarn/jora/demo/Controller/ProductController.java
+++ src/main/java/jarn/jora/demo/Controller/ProductController.java
@@ -1,9 +1,11 @@
 package jarn.jora.demo.Controller;
 
+import jarn.jora.demo.model.Product;
 import jarn.jora.demo.model.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,6 +14,11 @@ public class ProductController {
 
     @Autowired
     private ProductRepo repo;
+
+    @ModelAttribute("all")
+    public Iterable<Product> findAll(){
+        return repo.findAll();
+    }
 
     @RequestMapping(value = {"/","/index"},method = RequestMethod.GET)
     private String showIndex(ModelMap map){
