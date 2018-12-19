e737a5e07bf0762e2c0dace444b90afd626a57ec
JornRymenans <45791970+jornrymenans@users.noreply.github.com>
2018-12-19 10:41:15 +0100

    detailpagina + detailController

--- src/main/java/jarn/jora/demo/Controller/DetailController.java
+++ src/main/java/jarn/jora/demo/Controller/DetailController.java
@@ -5,27 +5,20 @@ import jarn.jora.demo.model.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ProductController {
+public class DetailController {
 
     @Autowired
     private ProductRepo repo;
 
-    @ModelAttribute("all")
-    public Iterable<Product> findAll(){
-        return repo.findAll();
+    @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
+    public String showDetails(ModelMap detailMap, @PathVariable(value = "id") int id){
+        Product prod = repo.findById(id).get();
+        detailMap.addAttribute("product", prod);
+        return "/details";
     }
-
-    @RequestMapping(value = {"/","/index"},method = RequestMethod.GET)
-    public String showIndex(ModelMap map){
-        return "index";
-    }
-
-
-
 }
--- src/main/java/jarn/jora/demo/model/ProductRepo.java
+++ src/main/java/jarn/jora/demo/model/ProductRepo.java
@@ -3,8 +3,10 @@ package jarn.jora.demo.model;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface ProductRepo extends CrudRepository<Product, Integer> {
 
     ArrayList<Product> findByCategory(Category category);
+
 }
